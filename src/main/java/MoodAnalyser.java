46d8107e292f897ee08456b604f3769049f70311
Darkdriller <swaindhruv28@gmail.com>
2023-12-07 01:17:06 +0530

    Added: Refactored Mood Analyser Test Case 2 Passed

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,9 +1,14 @@
+import java.util.HashMap;
+
 public class MoodAnalyser {
 
     public String analyseMood(String message){
         if(message.equals("This is a sad Message")){
             return "SAD";
         }
-        else return null;
+        if(message.equals("I am in Any Mood")){
+            return "HAPPY";
+        }
+        return null;
     }
 }
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -3,9 +3,15 @@ import org.junit.Assert;
 import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
-    public void testMoodAnalysis(){
+    public void testMoodAnalysisTC1(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("This is a sad Message");
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
+    }
+    @Test
+    public void testMoodAnalysisTC2(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
     }
 }

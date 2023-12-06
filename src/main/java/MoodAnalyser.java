c296671d47a4e340ac748d04fb70a629dd8e1e43
Darkdriller <swaindhruv28@gmail.com>
2023-12-07 01:21:58 +0530

    Added: Refactored Mood Analyser Constructer added Test Case 1 and 2 Passed

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -2,11 +2,18 @@ import java.util.HashMap;
 
 public class MoodAnalyser {
 
-    public String analyseMood(String message){
-        if(message.equals("This is a sad Message")){
+    String message;
+    public MoodAnalyser(String message){
+        this.message = message;
+    }
+    public MoodAnalyser(){
+        this.message = null;
+    }
+    public String analyseMood(){
+        if(this.message.equals("This is a sad Message")){
             return "SAD";
         }
-        if(message.equals("I am in Any Mood")){
+        if(this.message.equals("I am in Any Mood")){
             return "HAPPY";
         }
         return null;
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void testMoodAnalysisTC1(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a sad Message");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad Message");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
     @Test
     public void testMoodAnalysisTC2(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
     }
 }
